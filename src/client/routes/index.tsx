93f4adda70de20f4986dbdb2f698ee67ce5edf73
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-25 23:27:18 +0100

    Validate settings search route

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,9 +1,18 @@
-import { createRoute } from "@tanstack/react-router";
+import { createRoute, useSearch } from "@tanstack/react-router";
+import { isSystemSettingsTab, type SystemSettingsTab } from "../components/modals/system-settings-types";
 import { ProjectsPage } from "../pages/projects-page";
 import { rootRoute } from "./root";
 
 export const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
-  component: ProjectsPage
+  validateSearch: (search): { settings?: SystemSettingsTab } => ({
+    settings: isSystemSettingsTab(search.settings) ? search.settings : undefined
+  }),
+  component: IndexRouteComponent
 });
+
+function IndexRouteComponent() {
+  const search = useSearch({ from: indexRoute.id });
+  return <ProjectsPage settingsTab={search.settings} />;
+}
