4e8a816d4892306467c6120e32decb0549d0d8fc
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-30 07:36:14 +0100

    Remove manual onboarding route

--- src/client/routes/onboarding.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from "@tanstack/react-router";
-import { OnboardingPage } from "../pages/onboarding-page";
-import { rootRoute } from "./root";
-
-export const onboardingRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/onboarding",
-  component: OnboardingPage
-});
