ede02da023f9fda738208ea0a54dff2b9da370ba
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-30 07:35:01 +0100

    Add file-based root route

---
+++ src/client/routes/__root.tsx
@@ -0,0 +1,6 @@
+import { createRootRoute } from "@tanstack/react-router";
+import { RootShell } from "../components/layout/root-shell";
+
+export const Route = createRootRoute({
+  component: RootShell
+});
