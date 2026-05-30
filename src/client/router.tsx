0de0cdaf50985306cc1dee7613f42a893a65ec84
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-30 07:36:07 +0100

    Use generated route tree

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -1,13 +1,5 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { indexRoute } from "./routes/index";
-import { loginRoute } from "./routes/login";
-import { onboardingRoute } from "./routes/onboarding";
-import { onboardingSuccessRoute } from "./routes/onboarding-success";
-import { projectRoute } from "./routes/project";
-import { rootRoute } from "./routes/root";
-import { serviceIndexRoute, serviceTabRoute } from "./routes/service";
-
-const routeTree = rootRoute.addChildren([indexRoute, loginRoute, onboardingRoute, onboardingSuccessRoute, projectRoute, serviceIndexRoute, serviceTabRoute]);
+import { routeTree } from "./routeTree.gen";
 
 export const router = createRouter({
   routeTree,
