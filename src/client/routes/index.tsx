a57c53b4444fd365ad5bed33e87be6b4f58422ae
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-31 07:55:38 +0100

    Simplify projects index route

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,15 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { isSystemSettingsTab, type SystemSettingsTab } from "../components/modals/system-settings-types";
 import { ProjectsPage } from "../pages/projects-page";
 
 export const Route = createFileRoute("/")({
-  validateSearch: (search): { settings?: SystemSettingsTab } => ({
-    settings: isSystemSettingsTab(search.settings) ? search.settings : undefined
-  }),
   component: IndexRouteComponent
 });
 
 function IndexRouteComponent() {
-  const search = Route.useSearch();
-  return <ProjectsPage settingsTab={search.settings} />;
+  return <ProjectsPage />;
 }
